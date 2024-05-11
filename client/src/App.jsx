272bab350618e5291bb1b63b2dca6c05be6c5a87
f219292 <f219292@cfd.nu.edu.pk>
2024-05-11 17:41:53 +0500

    home code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import Signup from "./Component/Signup";
 import "./App.css";
 import Login from "./Component/Login";
 import Reset from "./Component/Reset";
+import Home from "./Component/Home";
 export default function App() {
   return (
     <>
   <Signup></Signup>
       <Login></Login>
       <Reset></Reset>
+      <Home></Home>
     </>
   );
 }
