f7f2424ef17ec19d13606b7b434c4038643f0fb1
f219274 <f219274@cfd.nu.edu.pk>
2024-05-11 15:19:35 +0500

    Login etc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Signup from "./Component/Signup";
 import "./App.css";
-import Login from "./Component/Login";
-import Reset from "./Component/Reset";
+
 export default function App() {
   return (
     <>
   <Signup></Signup>
-      <Login></Login>
-      <Reset></Reset>
+    
     </>
   );
 }
